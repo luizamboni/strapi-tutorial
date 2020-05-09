39594467e3f50ddad7235b0dd1a9bb4d939f2f6a
zamboni <luizamboni2002@hotmail.com>
2020-05-09 11:08:52 -0300

    ADD startAt and dateAte validation

--- api/page-content/services/page-content.js
+++ api/page-content/services/page-content.js
@@ -5,4 +5,28 @@
  * to customize this service
  */
 
-module.exports = {};
+const modelName = 'page-content'
+
+module.exports = {
+
+    _validate(data) {
+        if (!(data.startAt < data.endAt)){
+            throw new Error("startAt should be less than endAt date")
+        }
+    },
+
+    create(data, { files } = {}) {
+
+        this._validate(data);
+
+        return strapi.entityService
+          .create({ data, files }, { model: modelName })
+    },
+    
+    update(params, data, { files } = {}) {
+        
+        this._validate(data);
+        return strapi.entityService
+          .update({ params, data, files }, { model: modelName })
+    },   
+};
